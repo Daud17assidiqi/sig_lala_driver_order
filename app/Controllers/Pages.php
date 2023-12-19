92c557892131d160c45ba122f2e831c68809eef6
daudassidiqi <assidiqidaud@gmail.com>
2023-12-19 14:34:50 +0700

    view layouts

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -9,17 +9,36 @@ class Pages extends BaseController
         $data = [
             'title' => 'HOME'
         ];
-        echo view('layout/header', $data);
-        echo view('pages/home');
-        echo view('layout/footer');
+
+        return view('pages/home', $data);
     }
     public function about()
     {
         $data = [
             'title' => 'About Me'
         ];
-        echo view('layout/header', $data);
-        echo view('pages/about');
-        echo view('layout/footer');
+
+        return view('pages/about', $data);
+    }
+
+    public function contact()
+    {
+        $data = [
+            'title' => 'Contact Us',
+            'alamat' => [
+                [
+                    'tipe' => 'Rumah',
+                    'alamat' => 'Jl. jalan no. 1',
+                    'kota' => 'Bandung'
+                ],
+                [
+                    'tipe' => 'Kantor',
+                    'alamat' => 'Jl. Setiabudi no. 2',
+                    'kota' => 'Bandung'
+                ]
+            ]
+        ];
+
+        return view('pages/contact', $data);
     }
 }
